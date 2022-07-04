7962dfa6824ba438b953599d9e5fc847b1b6f9b8
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-07-04 07:24:36 -0300

    estruturando validacoes

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,18 +4,6 @@ import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 
 export async function cadastro(req, res) {
-    const infocadastro = req.body;
-    const {nome, email, senha} = req.body;
-    const cadastroSchema = joi.object({
-      nome: joi.string().required(),
-      email: joi.string().email().required(),
-      senha: joi.string().required(),
-      confirma: joi.ref('senha')
-    });
-    const {error} = cadastroSchema.validate(infocadastro, {abortEarly: false});
-    if(error){
-      return res.status(422).send(error);
-    }
     try {
       const salt = 10;
       await db.collection("usuarios").insertOne({
@@ -31,14 +19,6 @@ export async function cadastro(req, res) {
 }
 
 export async function login(req, res) {
-    const loginSchema = joi.object({
-        email: joi.string().email().required(),
-        senha: joi.string().required()
-    });
-    const {error} = loginSchema.validate(req.body, {abortEarly: false});
-    if(error){
-        return res.status(422).send(error);
-    }
     try {
         const usuario = await db.collection("usuarios").findOne({email: req.body.email});
         if(!usuario){
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ dotenv.config();
 const app = express();
 app.use(json());
 app.use(cors());
+
 app.use(authRouter);
 app.use(atividadesRouter);
 
